cfcb9e3bf0ca51f9f258d8d92b1e8ea387910d31
Manu Coutinho <gdn.sasha@gmail.com>
2026-09-21 20:24:49 -0300

    Initial commit

---
+++ src/pages/Home/Home.spec.ts
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom'
+import { describe, it, expect } from 'vitest'
+
+
+describe('<Home/>', () => {
+  it('should render example correctly', () => {
+    expect(true).toBe(true)
+  })
+})
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types='vitest' />
+/// <reference types='vite/client' />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import * as path from 'node:path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  },
+  setupFiles: ['./vitest-setup.js'],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, './src/') }]
+  }
+})
